a322801dcae01f951cd38aa57292a2a56e32c249
baldm0mma <jev.forsberg@gmail.com>
2019-12-18 12:07:39 -0700

    Add types file to sessions

--- src/Store/Session/types.ts
+++ src/Store/Session/types.ts
@@ -1,16 +1,1 @@
-export interface Movie {
-  id: number;
-  original_title: string;
-  favorited: boolean;
-}
 
-export interface User {
-  id: number;
-  email: string;
-  favorites: Movie[];
-}
-
-export interface SessionState {
-  loggedIn: boolean;
-  user: User;
-}
