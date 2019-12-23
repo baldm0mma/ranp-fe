1c7431cb3066765455fc0b98ae297fc4a9cb3b8b
baldm0mma <jev.forsberg@gmail.com>
2019-12-23 10:43:49 -0700

    Add movieReducer

--- src/Store/Actions/actionsIndex.ts
+++ src/Store/Actions/actionsIndex.ts
@@ -1,11 +1,12 @@
 import { Movie } from "../Types/movieTypes";
+import { AppActions } from "../Types/actionTypes";
 
-export const setMovies = (movies: Movie[]) => ({
+export const setMovies = (movies: Movie[]): AppActions => ({
   type: "SET_MOVIES",
   movies
 });
 
-export const toggleFavorite = id => ({
+export const toggleFavorite = (id: string): AppActions => ({
   type: "TOGGLE_FAVORITE",
   id
 });
---
+++ src/Store/Reducers/movieReducer.ts
@@ -0,0 +1,9 @@
+import { Movie } from "../Types/movieTypes";
+
+const defaultMovieState: Movie[] = [];
+
+export const movieReducer = (state = defaultMovieState, action) => ({
+  switch (action.type) {
+
+  }
+})
